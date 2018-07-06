55cd54d2b9bfe806e1c17af51977f3afeb622956
David <davidaapariciobak@gmail.com>
2018-07-06 15:34:08 -0400

    very nice looking but the cart still not working, what happens ?

--- erpfa/src/components/shelf/Product.js
+++ erpfa/src/components/shelf/Product.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+//iNNER coMPONENTS
 import Thumb from '../Thumb';
-
 import util from '../../Utils';
 
 const Product = ({product, addProduct}) => {
